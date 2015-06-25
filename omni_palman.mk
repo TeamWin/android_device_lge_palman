12a6443c0322721c68db03c80d89730d0e3ddd5b
fefifofum <jorsellis@gmail.com>
2015-06-25 18:37:05 +0200

    Rename storage labels and other updates
    Change-Id: I507f8c406276bfbe1ef3a3b0b7721bee14999152

--- omni_palman.mk
+++ omni_palman.mk
@@ -1,9 +1,9 @@
-# Inherit device configuration
-$(call inherit-product, device/lge/palman/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Device identifier. This must come after all inclusions
+PRODUCT_COPY_FILES += device/lge/palman/kernel:kernel
+
 PRODUCT_DEVICE := palman
 PRODUCT_NAME := omni_palman
-PRODUCT_BRAND := lge
+PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-V510
 PRODUCT_MANUFACTURER := LGE
