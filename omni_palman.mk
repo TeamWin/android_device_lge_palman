3fb8dd8160b6cd0eb3b4a591b3fc60771f10bf67
fefifofum <jorsellis@gmail.com>
2015-06-28 15:26:18 +0200

    PRODUCT_BRAND should be in lower case
    Change-Id: Ib682e6aab538565c60efab2395da03cd38c9e5e4

--- omni_palman.mk
+++ omni_palman.mk
@@ -4,6 +4,6 @@ PRODUCT_COPY_FILES += device/lge/palman/kernel:kernel
 
 PRODUCT_DEVICE := palman
 PRODUCT_NAME := omni_palman
-PRODUCT_BRAND := LGE
+PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-V510
 PRODUCT_MANUFACTURER := LGE
